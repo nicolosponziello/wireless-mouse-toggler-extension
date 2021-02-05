ea900db5198f76b1412a160e449b613d2cc5e1e0
nicolosponziello <nicolosponziello@gmail.com>
2021-02-05 09:34:08 +0100

    install: copy scripts in home folder

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ glib-compile-schemas ./schemas
 mkdir ~/.local/share/gnome-shell/extensions/mouse@nicolosponziello.gmail.com
 
 cp -r ./* ~/.local/share/gnome-shell/extensions/mouse@nicolosponziello.gmail.com
+
+cp enable-wireless-mouse.sh ~/
+cp disable-wireless-mouse.sh ~/
